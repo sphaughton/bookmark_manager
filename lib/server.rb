db8a75584af362742461c26d380e9ddb73b14e82
Sean Haughton <seanphaughton@gmail.com>
2015-02-24 17:42:25 +0000

    Managed links and tags

--- lib/server.rb
+++ lib/server.rb
@@ -19,5 +19,12 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
+  post '/links' do 
+    url = params["url"]
+    title = params["title"]
+    Link.create(:url => url, :title => title)
+    redirect to('/')
+  end
+
 end
 
