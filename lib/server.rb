f90de5dd782f2ce5238da4081f92d141ffb804a9
Sean Haughton <seanphaughton@gmail.com>
2015-02-25 16:00:45 +0000

    Refactored code

--- lib/server.rb
+++ lib/server.rb
@@ -48,6 +48,14 @@ class BookmarkManager < Sinatra::Base
     redirect to('/')
   end
 
+  helpers do
+
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
+
+  end
+
 end
 
 DataMapper.finalize
